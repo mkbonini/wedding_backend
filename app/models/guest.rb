c293198508bc1de871d7d60324391f02d72fa233
mkbonini <mkbonini@gmail.com>
2022-08-17 13:56:22 -0600

    add model validations and enums

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,8 @@
 class Guest < ApplicationRecord
   belongs_to :logging
   belongs_to :team
+
+  validates_presence_of :first_name, :last_name, :email, :rsvp, :diet, :meals, :payment_method, :arrival_date, :age, :plus_ones, :comments
+
+  enum rsvp: { "no": 0, "yes": 1 }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ApplicationRecord
+    has_many :guests
+
+    validates_presence_of :name, :theme
 end
