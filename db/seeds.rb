d154bdfbe767984dcd42bb20a10da258d46aa56c
mkbonini <mkbonini@gmail.com>
2022-08-17 17:12:34 -0600

    seed using factories for database testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+
+lodgings = FactoryBot.create_list(:lodging, 10)
+team = FactoryBot.create(:team)
+guests = FactoryBot.create_list(:guest, 10, lodging_id: lodgings.first.id, team_id: team.id) 
