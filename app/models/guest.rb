b59622b61b921d4ebe263b9f2c39da09a7552b6a
mkbonini <mkbonini@gmail.com>
2022-12-23 11:31:53 -0700

    remove restrictions from guest model creation.  Only first and last name is needed

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,10 +1,10 @@
 class Guest < ApplicationRecord
-  belongs_to :lodging
-  belongs_to :team
+  belongs_to :lodging, optional: true
+  belongs_to :team, optional: true
   has_many :kids
   has_many :plus_ones
 
-  validates_presence_of :first_name, :last_name, :email, :rsvp, :payment_method, :arrival_date, :plus_one_count
+  validates_presence_of :first_name, :last_name
 
   enum rsvp: { "no": 0, "yes": 1, "pending": 2 }
 end
