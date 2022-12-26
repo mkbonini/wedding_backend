e361d0f307b2f41a9012f011e33eaa2597957a7c
mkbonini <mkbonini@gmail.com>
2022-12-26 14:33:48 -0700

    Add guest deafult values to counters

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,8 +3,14 @@ class Guest < ApplicationRecord
   belongs_to :team, optional: true
   has_many :kids
   has_many :plus_ones
-
+  after_initialize :set_defaults
   validates_presence_of :first_name, :last_name
 
   enum rsvp: { "no": 0, "yes": 1, "pending": 2 }
+
+  private
+  def set_defaults
+    self.plus_one_count ||= 0 
+    self.party_count ||= 1
+  end
 end
