a1d3c621151fc2176d2334681729af3a5aebf18c
mkbonini <mkbonini@gmail.com>
2023-01-02 13:26:02 -0700

    Add Teams routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :kids
   resources :plus_ones
   resources :guests 
+  resources :teams
   get '/guest_list', to: 'guest_list#index'
   get '/guests/:id/email', to: 'guests#email'
   get '/dashboard', to: 'dashboard#index'
