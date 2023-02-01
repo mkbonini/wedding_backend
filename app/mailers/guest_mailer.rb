6a851caefef91d61d56040762bb26399affe638c
mkbonini <mkbonini@gmail.com>
2023-02-01 16:39:17 -0700

    Update: welcome email

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -3,7 +3,12 @@ class GuestMailer < ApplicationMailer
 
     def welcome_email
       @guest = params[:guest]
-
+      attachments.inline["Group_4.svg"] = File.read("#{Rails.root}/app/assets/images/Group_4.svg")
+      attachments.inline["Group_2.svg"] = File.read("#{Rails.root}/app/assets/images/Group_2.svg")
+      attachments.inline["instagram2x.png"] = File.read("#{Rails.root}/app/assets/images/instagram2x.png")
+      attachments.inline["mail2x.png"] = File.read("#{Rails.root}/app/assets/images/mail2x.png")
+      attachments.inline["website2x.png"] = File.read("#{Rails.root}/app/assets/images/website2x.png")
+      
       mail(to: @guest.email, subject: 'Thank you for your RSVP')
     end
 end
