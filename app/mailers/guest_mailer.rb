8b7455f746f698c95e8c7bc5543cc59146053f51
mkbonini <mkbonini@gmail.com>
2023-02-01 18:13:29 -0700

    update: email images from svg to png

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -3,12 +3,13 @@ class GuestMailer < ApplicationMailer
 
     def welcome_email
       @guest = params[:guest]
-      attachments.inline["Group_4.svg"] = File.read("#{Rails.root}/app/assets/images/Group_4.svg")
-      attachments.inline["Group_2.svg"] = File.read("#{Rails.root}/app/assets/images/Group_2.svg")
+      
+      attachments.inline["header.png"] = File.read("#{Rails.root}/app/assets/images/header.png")
+      attachments.inline["xoxo.png"] = File.read("#{Rails.root}/app/assets/images/xoxo.png")
       attachments.inline["instagram2x.png"] = File.read("#{Rails.root}/app/assets/images/instagram2x.png")
       attachments.inline["mail2x.png"] = File.read("#{Rails.root}/app/assets/images/mail2x.png")
       attachments.inline["website2x.png"] = File.read("#{Rails.root}/app/assets/images/website2x.png")
-      
+
       mail(to: @guest.email, subject: 'Thank you for your RSVP')
     end
 end
