8f9bd9127bed1fac324db161ecea3899adc4a207
mkbonini <mkbonini@gmail.com>
2023-02-06 17:01:42 -0700

    Update: notification email with guest details

--- spec/mailers/previews/guest_preview.rb
+++ spec/mailers/previews/guest_preview.rb
@@ -7,4 +7,12 @@ class GuestMailerPreview < ActionMailer::Preview
 
         GuestMailer.with(guest: guest).welcome_email
     end
+
+    def notification_email
+        lodgings = FactoryBot.create(:lodging)
+        team = FactoryBot.create(:team)
+        guest = FactoryBot.create(:guest, lodging_id: lodgings.id, team_id: team.id) 
+
+        GuestMailer.with(guest: guest).notification_email
+    end
 end
