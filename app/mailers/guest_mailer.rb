0e514900ac9c080abb66329776aa2201a04aa79f
mkbonini <mkbonini@gmail.com>
2023-02-13 15:13:04 -0700

    update: welcome email header

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -5,7 +5,6 @@ class GuestMailer < ApplicationMailer
       @guest = params[:guest]
       
       attachments.inline["header.png"] = File.read("#{Rails.root}/app/assets/images/header.png")
-      attachments.inline["xoxo.png"] = File.read("#{Rails.root}/app/assets/images/xoxo.png")
       attachments.inline["instagram2x.png"] = File.read("#{Rails.root}/app/assets/images/instagram2x.png")
       attachments.inline["mail2x.png"] = File.read("#{Rails.root}/app/assets/images/mail2x.png")
       attachments.inline["website2x.png"] = File.read("#{Rails.root}/app/assets/images/website2x.png")
