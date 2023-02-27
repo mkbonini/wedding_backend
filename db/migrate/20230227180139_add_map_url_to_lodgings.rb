cafcb69e2d172f090a309c85c1307fe28ab84a2f
mkbonini <mkbonini@gmail.com>
2023-02-27 11:04:01 -0700

    Add map_url to lodging table

---
+++ db/migrate/20230227180139_add_map_url_to_lodgings.rb
@@ -0,0 +1,5 @@
+class AddMapUrlToLodgings < ActiveRecord::Migration[5.2]
+  def change
+    add_column :lodgings, :map_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_22_182111) do
+ActiveRecord::Schema.define(version: 2023_02_27_180139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2023_02_22_182111) do
     t.string "title"
     t.string "image_url"
     t.string "color"
+    t.string "map_url"
   end
 
   create_table "plus_ones", force: :cascade do |t|
